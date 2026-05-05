e6c21dd640e471e4646a43fe225cbea01926d775
Tirthraj Mahajan <39341897+tirthraj07@users.noreply.github.com>
2026-05-06 02:12:58 +0530

    tut-01: hello world

---
+++ 01-hello-world/hello_world/main/hello_world.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void app_main(void) {
+    while(1){
+        printf("Hello World! I am Tirthraj Mahajan! \n");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
